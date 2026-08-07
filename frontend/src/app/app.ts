47426fdf12ac665b314b437fb1c9c0495537217d
Jaro29 <jarofieducik@gmail.com>
2026-08-07 14:06:30 +0200

    Move search to App component, add collapsible add-book form and clear-search button

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { BookList } from './components/book-list/book-list';
-import { RouterOutlet } from "@angular/router";
+import { RouterOutlet } from '@angular/router';
 import { AddBookForm } from './components/add-book-form/add-book-form';
+import { BookService } from './services/book';
 
 @Component({
   selector: 'app-root',
-  imports: [BookList, RouterOutlet, AddBookForm],  // dodaj tutaj
+  imports: [BookList, RouterOutlet, AddBookForm], // dodaj tutaj
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
-export class App {}
+export class App {
+  showAddForm = signal(false);
+
+  protected bookService = inject(BookService);
+
+  onSearch(query: string) {
+    this.bookService.searchQuery.set(query);
+    this.bookService.loadBooks();
+  }
+
+  clearSearch() {
+    this.bookService.searchQuery.set("");
+    this.bookService.loadBooks();
+  }
+
+  toggleAddForm() {
+    this.showAddForm.update((value) => !value);
+  }
+}
